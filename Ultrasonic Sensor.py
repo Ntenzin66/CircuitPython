2775f377fbafda869bc4376907df90f84eb039e7
Ngawang Tenzin <ntenzin66@ccs.local>
2021-09-20 09:48:00 -0400

    More Ultrasonic Sensor Code

--- Ultrasonic Sensor.py
+++ Ultrasonic Sensor.py
@@ -1,25 +1,27 @@
 import board
-import neopixel
 import time
 import adafruit_hcsr04
+import neopixel
 sonar = adafruit_hcsr04.HCSR04(trigger_pin=board.D3, echo_pin=board.D2)
-dot = neopixel.Neopixel(board.NEOPIXEL, 1)
-dot.brightness = .1
-
-
+dot = neopixel.NeoPixel(board.NEOPIXEL, 1)
+pos = 1;
+print("new code! 2")
+time.sleep(1)
 
 while True:
-        try:
-            print("sonar.distance")
-        except RuntimeError:
-            print("Retrying!")
-        time.sleep(0.1)
+    try:
+        pos = sonar.distance
+        print((pos))
+    except RuntimeError:
+        print("Retrying!")
+    time.sleep(0.01)
 
-def distance(pos)
-
-
-
-
-
-
-
+    if pos < 5 or pos > 20:
+        r = 255
+        b = 0
+        g = 0
+    else:
+        r = 0
+        b = 255
+        g = 0
+    dot.fill((r,g,b))
