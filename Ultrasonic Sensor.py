3c57db099229c9e2dd1c0910fec19f875a63c974
Ngawang Tenzin <ntenzin66@ccs.local>
2021-09-24 09:35:17 -0400

    Final code for Ultrasonic Sensor

--- Ultrasonic Sensor.py
+++ Ultrasonic Sensor.py
@@ -4,10 +4,10 @@ import adafruit_hcsr04
 import neopixel
 sonar = adafruit_hcsr04.HCSR04(trigger_pin=board.D3, echo_pin=board.D2)
 dot = neopixel.NeoPixel(board.NEOPIXEL, 1)
-pos = 1;
+pos = 1
 print("new code! 2")
 time.sleep(1)
-dot.brightness = .1
+dot.brightness = 0.1
 
 while True:
     try:
@@ -17,7 +17,7 @@ while True:
         print("Retrying!")
     time.sleep(0.01)
 
-      if pos < 5:
+    if pos < 5:
         r = 255
         b = 0
         g = 0
@@ -27,8 +27,8 @@ while True:
         b = (pos-5)/15*255
     elif pos < 35:
         r = 0
-        g = (pos-5)/15*255
-        b = 255-((pos-5)/15*255)
+        g = (pos-20)/15*255
+        b = 255-((pos-20)/15*255)
     else:
         r = 0
         g = 255
