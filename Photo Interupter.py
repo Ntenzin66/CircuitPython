6d121d42e2ea767602b961aa2f7f50dc0e23aafe
Ngawang Tenzin <ntenzin66@ccs.local>
2021-10-01 09:33:11 -0400

    More code for photo interrupter

--- Photo Interupter.py
+++ Photo Interupter.py
@@ -6,8 +6,11 @@ import digitalio
 pin = digitalio.DigitalInOut(board.D4)
 pin.direction = digitalio.Direction.INPUT
 pin.pull = digitalio.Pull.UP
+initial = time.monotonic()
 
-    while True:
-
-
-
+while True:
+    now = time.monotonic()
+    if now - initial > 4.000:
+        print("done")
+    else:
+        initial = now
